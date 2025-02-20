4b1fdb9c5e208de3b4cbf4257453423ae072ea3a
Prateek-Fst <147906999+Prateek-Fst@users.noreply.github.com>
2025-02-20 18:30:15 +0530

    updated port leakes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allow headers
   })
-
-  await app.listen(3001)
+  const PORT = process.env.PORT || 3001;
+  await app.listen(PORT)
 }
 bootstrap()
